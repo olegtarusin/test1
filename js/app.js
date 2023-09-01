a1f6a1b6c816c0eface8413b21f7cd4f79377adb
olegtarusin <131518184+olegtarusin@users.noreply.github.com>
2023-09-01 19:24:09 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,66 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+        }
+    };
+    function addTouchClass() {
+        if (isMobile.any()) document.documentElement.classList.add("touch");
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    "use strict";
+    function addClasser(ourElem) {
+        document.addEventListener("touchstart", clickseeker);
+        document.addEventListener("touchend", clickseeker);
+        function clickseeker(event) {
+            const targetElem = event.target;
+            if (targetElem.closest(ourElem)) {
+                const hoverElem = targetElem.closest(ourElem);
+                if (event.type == "touchstart") hoverElem.classList.add("clicked");
+                if (event.type == "touchend") hoverElem.classList.remove("clicked");
+            }
+        }
+    }
+    addClasser(".button");
+    addClasser(".prising__item");
+    window["FLS"] = true;
+    isWebp();
+    addTouchClass();
+})();
